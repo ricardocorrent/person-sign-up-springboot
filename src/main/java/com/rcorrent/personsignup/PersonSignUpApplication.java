f8fb2fb2007482693e5d64801f1ec5e2ebfa3768
Ricardo Corrent <ricardo.corrent@wssim.com.br>
2019-07-17 09:08:42 -0300

    (2019-07-17) First commit, creating spring project

---
+++ src/main/java/com/rcorrent/personsignup/PersonSignUpApplication.java
@@ -0,0 +1,13 @@
+package com.rcorrent.personsignup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonSignUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonSignUpApplication.class, args);
+	}
+
+}
